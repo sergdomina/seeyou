c568f10ee72e1d633b9347263982996baaca9471
dykyys <dykyy.s@gmail.com>
2022-03-14 11:17:16 +0200

    update media

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import { device, size } from '../../constants/mediaSize';
-console.log(device.mobile);
+
 export const Container = styled.div`
   padding: 0px 15px;
   margin: 0 auto;
   width: 100%;
   outline: 2px solid red;
+
   ${device.mobile} {
     width: ${size.mobile};
   }
