228338ba4292842d7b33099b4d02b9473869a141
dykyys <dykyy.s@gmail.com>
2022-03-18 16:55:20 +0200

    add support

--- src/components/Support/Support.js
+++ src/components/Support/Support.js
@@ -1,15 +1,26 @@
-import { SupportTitle, SupportDiscription } from './Support.styled';
-import GlobalSection from '../Section';
+import {
+  SupportTitle,
+  SupportDiscription,
+  SupportSection,
+  SupportContainer,
+} from './Support.styled';
+
+import Container from '../Container';
+
 const Support = () => {
   return (
-    <GlobalSection>
-      <SupportTitle>Support Ukraine Banner</SupportTitle>
-      <SupportDiscription>
-        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
-        sint. Velit officia consequat duis enim velit mollit. Exercitation
-        veniam consequat sunt nostrud amet.
-      </SupportDiscription>
-    </GlobalSection>
+    <SupportSection>
+      <Container>
+        <SupportContainer>
+          <SupportTitle>Support Ukraine Banner</SupportTitle>
+          <SupportDiscription>
+            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+            sint. Velit officia consequat duis enim velit mollit. Exercitation
+            veniam consequat sunt nostrud amet.
+          </SupportDiscription>
+        </SupportContainer>
+      </Container>
+    </SupportSection>
   );
 };
 export default Support;
--- src/components/Support/Support.styled.js
+++ src/components/Support/Support.styled.js
@@ -1,30 +1,33 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const SupportTitle = styled.h1`
-font-family: 'Jost';
-font-weight: 600;
-font-size: 30px;
-line-height: 1,5px;
-text-align: center;
-margin-bottom: 16px;
-color: ${({ theme }) => theme.colors.black};
+export const SupportSection = styled.h2`
+  padding-top: 101px;
+  padding-bottom: 88px;
+  background: ${({ theme }) => theme.colors.blue};
 `;
 
-export const SupportThumb = styled.div`
-width: 271px;
-height: 238px;
-border-radius: 13px;
-margin: 0 auto;
-margin-bottom: 40px;
-background: ${({ theme }) => theme.colors.blue};
+export const SupportTitle = styled.h2`
+  font-family: 'Jost';
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 1.5;
+  margin-bottom: 16px;
+  color: ${({ theme }) => theme.colors.black};
 `;
-export const SupportDiscription= styled.p`
-font-family: 'Open Sans';
-font-style: normal;
-font-weight: 400;
-font-size: 14px;
-line-height: 150%;
-text-align: center;
 
-color: ${({ theme }) => theme.colors.primaryText};
+export const SupportContainer = styled.div`
+  text-align: center;
+  ${({ theme }) => theme.device.desktop} {
+    width: 380px;
+    text-align: left;
+  }
+`;
+export const SupportDiscription = styled.p`
+  font-family: 'Open Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.5;
+
+  color: ${({ theme }) => theme.colors.primaryText};
 `;
