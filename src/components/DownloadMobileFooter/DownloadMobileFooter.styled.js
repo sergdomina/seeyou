ccae89e8caac31707bc314b8b4799f87a0ef5281
Sergey Domina <serg@seeyou.eu>
2022-05-04 08:11:50 +0100

    04_05

--- src/components/DownloadMobileFooter/DownloadMobileFooter.styled.js
+++ src/components/DownloadMobileFooter/DownloadMobileFooter.styled.js
@@ -5,11 +5,12 @@ export const DownloadLinkBox = styled.div`
   width: 100%;
   transform: translateX(-50%);
   left: 50%;
-  padding-top: 8px;
+  padding-top: 30px;
   padding-bottom: 34px;
   text-align: 'center';
   position: fixed;
   bottom: 0;
+  box-shadow: 2px 0px 8px rgba(0, 0, 0, 0.25);
   background: ${({ theme }) => theme.colors.white};
   /* z-index: 1; */
 `;
