1088dfaa55044843e14b37388406dc060c8fe89e
dykyys <dykyy.s@gmail.com>
2022-05-16 14:55:53 +0300

    fix zoom

--- src/components/PopupHeader/PopupHeader.js
+++ src/components/PopupHeader/PopupHeader.js
@@ -3,17 +3,7 @@ import { Popup } from 'semantic-ui-react';
 import termsOfService from '../../PDF/SEEYOU_TERMS_OF_SERVICE.pdf';
 import privacyStatement from '../../PDF/SEEYOU_PRIVACY_STATEMENT.pdf';
 
-
-
 const PopupHeader = () => (
-
-  useOnClickOutside(node, () => toggleMenu(false), isOpenMobileMenu);
-
-  const [showModal, setShowModal] = useState(false);
-  const toggleModal = () => setShowModal(!showModal);
-  const { pathname } = useLocation();
-  const isDownloadPage = pathname !== '/providers-zoom';
-
   <Popup
     content={
       <PopupBox>
@@ -23,12 +13,7 @@ const PopupHeader = () => (
         <PopLink href={privacyStatement} target="_blank">
           Privacy Policy
         </PopLink>
-        <Link to='/providers-zoom' onClick={toggleMenu} >
-          Zoom
-        </Link>
-        <Link >
-          Privacy Policy
-        </Link>
+        <Link to="/providers-zoom"> Providers Zoom</Link>
       </PopupBox>
     }
     on={['focus', 'hover']}
@@ -36,8 +21,45 @@ const PopupHeader = () => (
     pinned
     hoverable
     style={{ zIndex: '2' }}
-    trigger={<PopButton>More</PopButton>}
+    trigger={<PopButton>Terms & Conditions</PopButton>}
   />
 );
 
 export default PopupHeader;
+
+// const PopupHeader = () => (
+
+//   useOnClickOutside(node, () => toggleMenu(false), isOpenMobileMenu);
+
+//   const [showModal, setShowModal] = useState(false);
+//   const toggleModal = () => setShowModal(!showModal);
+//   const { pathname } = useLocation();
+//   const isDownloadPage = pathname !== '/providers-zoom';
+
+//   <Popup
+//     content={
+//       <PopupBox>
+//         <PopLink href={termsOfService} target="_blank">
+//           Terms of service
+//         </PopLink>
+//         <PopLink href={privacyStatement} target="_blank">
+//           Privacy Policy
+//         </PopLink>
+//         <Link to='/providers-zoom' onClick={toggleMenu} >
+//           Zoom
+//         </Link>
+//         <Link >
+//           Privacy Policy
+//         </Link>
+//       </PopupBox>
+//     }
+//     on={['focus', 'hover']}
+//     position="bottom left"
+//     pinned
+//     hoverable
+//     style={{ zIndex: '2' }}
+//     trigger={<PopButton>More</PopButton>}
+//   />
+// );
+
+// export default PopupHeader;
